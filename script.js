2c92d0d67cbe58530a3bfe48a97f6142631a061a
kaylakarim <143722337+kaylakarim@users.noreply.github.com>
2025-08-20 22:53:46 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -86,6 +86,42 @@ $("#image-viewer .close").click(function(){
 $(window).on('unload', function() {
    $(window).scrollLeft(0);
 });	
+
+/*$(".imageGrid2025 .gridImage").each(function(){
+
+  var width = $(this).outerWidth(),
+      height = $(this).outerHeight();
+
+  if(height > width){
+      $(this).css({gridRow:"span 2"})
+  }
+})*/
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+// Get all images and insert the clicked image inside the modal
+// Get the content of the image description and insert it inside the modal image caption
+var images = document.getElementsByTagName('img');
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+var i;
+for (i = 0; i < images.length; i++) {
+  images[i].onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    modalImg.alt = this.alt;
+    captionText.innerHTML = this.nextElementSibling.innerHTML;
+  }
+}
 	
 	
 		
